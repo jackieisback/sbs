f164c6d420f70aebac0dda8019d339f5761bdc34
Jackie <jackie@isback.com>
2017-05-30 21:57:33 +0200

    make zip more generic, is also working when intro.txt is open

--- sbs_zip.py
+++ sbs_zip.py
@@ -3,19 +3,24 @@ import os
 # path to 7zip exe
 path_to_7zip = r'"C:\Program Files\7-Zip\7zG.exe" '
 
+# get current working file (sb.txt or intro.txt)
+working_file = notepad.getCurrentFilename()
+
+# get current working folder
+working_folder = os.path.normpath(os.path.join(working_file,'../'))
+
 # get the path to the current file
-path_to_sb_txt = notepad.getCurrentFilename()
+path_to_sb_txt = "\"" + working_folder + "\\sb.txt" + "\""
 
 # get path to intro.txt file
-path_to_intro_txt = "\"" + os.path.normpath(os.path.join(path_to_sb_txt,'../')) + "\\intro.txt" + "\""
+path_to_intro_txt = "\"" + working_folder + "\\intro.txt" + "\""
 
 # path to pic directory
-pic_dir = "\"" + os.path.normpath(os.path.join(path_to_sb_txt,'../')) + "\\pic" + "\""
+pic_dir = "\"" + working_folder + "\\pic" + "\""
 
 # name of .zip file
-output_file = "\"" + os.path.normpath(os.path.join(path_to_sb_txt,'../')) + "\\sb.zip" + "\""
+output_file = "\"" + working_folder + "\\sb.zip" + "\""
 
-path_to_sb_txt = "\"" + path_to_sb_txt + "\""
 # built command string
 commandstring = path_to_7zip + "a " + output_file + " " + path_to_sb_txt + " " + path_to_intro_txt + " " + pic_dir + "\\*"
 
