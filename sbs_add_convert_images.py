7696ccccc585a13fae6456917b713942e06d1735
jackieisback <1602@gmx.li>
2025-02-28 16:44:45 +0100

    Changed to NConvert

--- sbs_add_convert_images.py
+++ sbs_add_convert_images.py
@@ -5,16 +5,17 @@ import os
 import re
 
 # parts that need to be adapted
-snapshot_dir = r"C:\Users\XXX\Pictures"
-path_to_irfanview = r'"C:\Program Files (x86)\IrfanView\i_view64.exe" '
+snapshot_dir = r"C:\Users\Jackie500\Pictures"
+path_to_irfanview = r'"D:\01_Tools\NConvert\nconvert.exe" '
 
 numInts = 4
+useMovieFolderForImages = 1
 
 def getFilesInDir(dir, picType):
-  # get all files in directory
-  files_in_folder = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
-  # return only files with .png ending
-  return [i for i in files_in_folder if i.endswith(picType)]
+    # get all files in directory
+    files_in_folder = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
+    # return only files with .png ending
+    return [i for i in files_in_folder if i.endswith(picType)]
 
 def getPicDir():
     # get the path to the current file
@@ -33,10 +34,7 @@ def getLatestNumberInPicFolder():
 
     rel_files = getFilesInDir(pic_dir, ".jpg")
 
-    #print rel_files
-
     if rel_files:
-        #print rel_files[-1]
         latestNumber = re.findall(r'\d+', rel_files[-1])
     else:
         latestNumber = ["0000"]
@@ -50,25 +48,20 @@ def buildImageString(latestNumber, numZeros):
         numZeros -= 1
 
     latestNumber += 1
-    #print zeros
-    #print latestNumber
+
     return '[' + 'img' + '-' + zeros + str(latestNumber) + '.jpg' + ']'
 
 
 def getImageString():
-    text = "bla"
     latestNumber = ""
 
     # get latest number in pic folder
     latestNumber = getLatestNumberInPicFolder()
     # caluclate number of zeros
     numZeros = numInts - len(str(int(latestNumber)+1))
-    print latestNumber
-    print numZeros
     # build image string
     ImageString = buildImageString(int(latestNumber), numZeros)
 
-    #print ImageString
     return ImageString
 
 def createImage(file, ImageString):
@@ -80,11 +73,27 @@ def createImage(file, ImageString):
     # pic directory
     pic_dir = getPicDir()
 
-    commandstring = path_to_irfanview + filestring + r' /resize_long=350 /aspectratio /convert=' + pic_dir + "\\" + ImageString[1:-1] + "\""
+    # get current filename
+    filename = os.path.basename(notepad.getCurrentFilename())
+
+    if "sb.txt" in filename:
+        resize = "-resize 390 0 "
+    else:
+        resize = "-resize 500 0 "
+
+    #commandstring = path_to_irfanview + filestring + resize + r' /aspectratio /convert=' + pic_dir + "\\" + ImageString[1:-1] + "\""
+    commandstring = path_to_irfanview + "-out jpeg " + "-o " + "\"" + pic_dir + "\\" + ImageString[1:-1] + "\"" + " -ratio " + resize + filestring
+	
     console.run(commandstring)
 
 def moveFile(file, snapshot_dir):
-    newFolder = snapshot_dir + "\\move\\"
+    if useMovieFolderForImages == 1:
+        # get the name of the movie folder
+        path = os.path.normpath(notepad.getCurrentFilename())
+        newFolder = snapshot_dir + "\\" + path.split(os.sep)[-2] + "\\"
+    else:
+    # use default folder called move
+        newFolder = snapshot_dir + "\\move\\"
 
     # check if new folder already exists
     isExist = os.path.exists(newFolder)
@@ -110,4 +119,4 @@ for idx, file in enumerate(newImages):
     createImage(file, ImageString)
     # move file
     moveFile(file, snapshot_dir)
-
+	
